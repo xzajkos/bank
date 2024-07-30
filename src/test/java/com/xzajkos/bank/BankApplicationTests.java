2e58ddddcdd529a5cc8a7a2ee68d8963ffe9a8a0
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-07-30 15:25:12 +0200

    :tada: Initial commit

---
+++ src/test/java/com/xzajkos/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzajkos.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
