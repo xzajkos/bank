5a9fc77d218458924fa6d94ada9d2828c9e5e843
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-08-02 17:13:37 +0200

    Add 'SavingAccount' extending from 'Account

---
+++ src/main/java/com/xzajkos/bank/Model/SavingAccount.java
@@ -0,0 +1,12 @@
+package com.xzajkos.bank.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SavingAccount extends Account{
+}
