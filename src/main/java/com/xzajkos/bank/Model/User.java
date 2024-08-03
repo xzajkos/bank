8cf35c90bc5432c5b293ca250ecf97b1e3872098
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-08-03 11:37:18 +0200

    Add 'Role' field

--- src/main/java/com/xzajkos/bank/Model/User.java
+++ src/main/java/com/xzajkos/bank/Model/User.java
@@ -25,6 +25,7 @@ public class User implements UserDetails {
     private String lastName;
     private String username;
     private String password;
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
