ffe6a047534773d82810d70dcdade5143dffe325
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-08-03 11:38:22 +0200

    :zap: Add 'AccountRepository' interface

---
+++ src/main/java/com/xzajkos/bank/Repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.xzajkos.bank.Repository;
+
+import com.xzajkos.bank.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByAccountNumber(String accountNumber);
+}
