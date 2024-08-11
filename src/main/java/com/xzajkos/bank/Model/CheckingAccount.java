bd6871661dd0573a465e869d094c2e26c8c9ae5d
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-08-11 10:32:52 +0200

    :fire: Delete useless imports

--- src/main/java/com/xzajkos/bank/Model/CheckingAccount.java
+++ src/main/java/com/xzajkos/bank/Model/CheckingAccount.java
@@ -1,9 +1,6 @@
 package com.xzajkos.bank.Model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
