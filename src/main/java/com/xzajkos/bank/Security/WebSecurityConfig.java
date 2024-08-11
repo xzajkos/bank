caae3bf2f5a17f7ee88cf3ecfcf0d6d95f446255
Kacper Zajkowski <kacperzajkowski@icloud.com>
2024-08-11 11:13:38 +0200

    :sparkles: Add 'WebSecurityConfig'

---
+++ src/main/java/com/xzajkos/bank/Security/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.xzajkos.bank.Security;
+
+import com.xzajkos.bank.Filter.AuthenticationFilter;
+import com.xzajkos.bank.Service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final AuthenticationFilter authenticationFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public WebSecurityConfig(AuthenticationFilter authenticationFilter, UserDetailsServiceImpl userDetailsService) {
+        this.authenticationFilter = authenticationFilter;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.requestMatchers("/login", "/register")
+                        .permitAll()
+                        .requestMatchers("/admin/**")
+                        .hasAuthority("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(userDetailsService)
+                .build();
+    }
+}
